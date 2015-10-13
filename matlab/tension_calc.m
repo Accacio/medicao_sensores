e682a754adb27752c336b2c3c646f3507c2f067c
Accácio <raccacio2@gmail.com>
2015-10-13 17:14:52 -0300

    Fix commentary

--- matlab/tension_calc.m
+++ matlab/tension_calc.m
@@ -11,9 +11,9 @@ function [ Fcf, phi ] = tension_calc( Dca,phi_step,Dcf_div,subp_arrow,subp_pos)
 
 %Global variables
 global shoulder_phi;   %initial position of shoulders angle
-global arm_yini;        %high of the sholder from the origin
-global arm_xini;        %distace of the shoulder from the origin
-global forearm_yini;    
+global arm_yini;        %height of the sholder from the origin
+global arm_xini;        %distance of the shoulder from the origin
+global forearm_yini;
 global forearm_xini;
 global La;    %arm lenght in mts.
 global Lf;    %forearm length in mts.
@@ -23,9 +23,9 @@ global Dd;
 
 %Initial position of the arm
 shoulder_phi=pi;   %initial position of shoulders angle
-arm_yini=0;        %high of the sholder from the origin
-arm_xini=0;        %distace of the shoulder from the origin
-forearm_yini=arm_yini+La*cos(shoulder_phi);    
+arm_yini=0;        %height of the sholder from the origin
+arm_xini=0;        %distance of the shoulder from the origin
+forearm_yini=arm_yini+La*cos(shoulder_phi);
 forearm_xini=arm_xini+La*sin(shoulder_phi);
 
 %definitions about the human arm and disturbance
@@ -49,11 +49,11 @@ Llc=0.05;   %load cell length in mts.
 %variables
 %Dca=0.10;      % Clamping arm distance in mts.
 %Dcf=0.10;      % Clamping forearm distance in mts.
-lw=0;           % lenght of work in mts.
+x_max=0;           % lenght of work in mts.
 teta_fc=[];     % forearm clamping angle in radians.
 teta_ac=[];     % arm clamping angle in radians
 phi=0;          % elbow angle in radians.
-max_phi_open=2.1679; %150 grados
+max_phi_open=2.1679; %150 degrees
 
 %forearm variables
 Wf=per_fw*Sw;       %weight of the forearm in kg.
@@ -61,21 +61,21 @@ Dfcm=per_fcm*Lf;    %distance of the center of mass in the forearm in mts.
 
 %disturbance variables
 Wd=per_hw*Sw+Wd_object; % weight of the disturbance included weight of the hand
-Dd=Lf+per_hcm*Lh;       %ditance of the disturbance from the elbow
+Dd=Lf+per_hcm*Lh;       %distance of the disturbance from the elbow
 
 %work lenghts maximums to work
-lw=La-Dca-(Ce+Lp+Llc);
-if(lw>0)
-    aux_teta_cf=asin((Dca*sin(max_phi_open))/lw);
+x_max=La-Dca-(Ce+Lp+Llc);
+if(x_max>0)
+    aux_teta_cf=asin((Dca*sin(max_phi_open))/x_max); %sin law
     aux_teta_ca=pi-max_phi_open-aux_teta_cf;
-    Dcf_max=(lw*sin(aux_teta_ca))/sin(max_phi_open);
+    Dcf_max=(x_max*sin(aux_teta_ca))/sin(max_phi_open);
 
     Dcf_step=Dcf_max/Dcf_div;
     phi=[0];
     Fcf=[0];
     Dcf=[0];
     x=[0];
-    
+
     i=0;
     phi_val=['elbow ang='];
     leg_str=[0];
@@ -84,18 +84,18 @@ if(lw>0)
         i=i+1;
         phi(i)=phi_aux
         %leg_str(i,1)=num2str(phi(i)*180/pi)
-        
+
         for j=1:Dcf_div
             Dcf(j,1)=Dcf_max-(j-1)*Dcf_step;
-            
-            %calculation of tensor 
+
+            %calculation of tensor
             x(j,i)=sqrt(Dca^2+Dcf(j,1)^2-2*Dca*Dcf(j,1)*cos(phi(i)));
-            
+
             %angles calculations
             teta_fc(j,i)=asin((Dca*sin(phi(i)))/x(j,i));
             teta_ac(j,i)=asin((Dcf(j,1)*sin(phi(i)))/x(j,i));
 
-    
+
             %Clamping force calculation
             Tfc=Wf*Dfcm*g*sin(phi(i));
             Td=Wd*Dd*g*sin(phi(i));
